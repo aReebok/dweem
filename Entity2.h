aad8d6967e5226d0c11fd8c2153528a0a9f53a18
Nick Edwards <edward7@stolaf.edu>
2019-12-05 18:45:29 -0600

    Entity2.h

--- Entity2.h
+++ Entity2.h
@@ -1,3 +1,6 @@
+#ifndef _ENTITY2_H_
+#define _ENTITY2_H_
+
 #include<iostream>
 using namespace std;
 #include<vector>
@@ -7,8 +10,8 @@ class Entity{
     int xp;
     int health;
     int stamina;
-    float speed;
     float strength;
+    float speed;
     float dexterity;
     float intelligence;
     float wisdom;
@@ -23,29 +26,25 @@ class Entity{
 public:
     Entity() : level(1), xp(0), health(10), stamina(10), speed(5), strength(2), dexterity(0), intelligence(5), wisdom(5), charisma(2), NPC(true), enemy(false), boots(false), chest_plate(false), helmet(false), leggings(false), weapon(false) {}
 
-    Entity(float str, float dex, float intel, float wis, float ch, float sp) : level(1), xp(0), health(10), stamina(10), speed(sp), strength(str), dexterity(dex), intelligence(intel), wisdom(wis), charisma(ch), NPC(false), enemy(false), carrying_capacity(5), boots(false), chest_plate(false), helmet(false), leggings(false), weapon(false) {}
+    Entity(float str, float dex, float intel, float wis, float ch, float sp) : level(1), xp(0), health(10), stamina(10), speed(sp), strength(str), dexterity(dex), intelligence(intel), wisdom(wis), charisma(ch), NPC(false), enemy(false), boots(false), chest_plate(false), helmet(false), leggings(false), weapon(false) {}
 
+    void set_strength(int s);
 };
 
 class Human : public Entity{
     string race = "Human";
     vector <float> stats;
-    vector <float> special_stats = {2,1,1,1,0,1};
+    vector <float> special_stats = {2,1,1,1,0,1}; //{Strength, Speed, Dexterity, Intelligence, Wisdom, Charisma}
+    
 public:
-    Human() : Entity() {}
+    Human() {}
 
     Human(float str, float dex, float intel, float wis, float ch, float sp){
         stats = {str, dex, intel, wis, ch, sp};
     }
 
-    void create_human(float str, float dex, float intel, float wis, float ch, float sp)
-    {
-        stats = {str, dex, intel, wis, ch, sp};
-        for (int i = 0; i < 5; i++)
-        {
-            stats[i] += special_stats[i];
-        }
-    }
+    void create_human(float str, float dex, float intel, float wis, float ch, float sp);
+
 
     // Use map weapon map<string, float, float> weapon
     // weapon["Sword0"] = 3, 0
@@ -53,33 +52,35 @@ public:
     // weapon["Hammer"] = 5, 2
 };
 
-class Dwarf : public Entity{
-    string race = "Dwarf";
-public:
-    Dwarf() : Entity() {};
+// class Dwarf : public Entity{
+//     string race = "Dwarf";
+// public:
+//     Dwarf() : Entity() {};
 
-    Dwarf(float str, float dex, float con, float intel, float wis, float ch, float sp){
-        Entity(str, dex, intel, wis, ch, sp);
-    }
-};
+//     Dwarf(float str, float dex, float con, float intel, float wis, float ch, float sp){
+//         Entity(str, dex, intel, wis, ch, sp);
+//     }
+// };
 
-class Elf : public Entity{
-    string race = "Elf";
-public:
-    Elf() : Entity() {};
+// class Elf : public Entity{
+//     string race = "Elf";
+// public:
+//     Elf() : Entity() {};
 
-    Elf(float str, float dex, float con, float intel, float wis, float ch, float sp){
-        Entity(str, dex, intel, wis, ch, sp);
-    }
-};
+//     Elf(float str, float dex, float con, float intel, float wis, float ch, float sp){
+//         Entity(str, dex, intel, wis, ch, sp);
+//     }
+// };
 
-class Ogre : public Entity{
-    string race = "Ogre";
-public:
+// class Ogre : public Entity{
+//     string race = "Ogre";
+// public:
 
-    Ogre() 
+//     Ogre() 
 
-    Ogre(int lvl) {
+//     Ogre(int lvl) {
         
-    }
-};
+//     }
+// };
+
+#endif //_ENTITY_H_
