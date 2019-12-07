64dd968aacb2d6dc6bb2b27d5a3fac8a9a20e839
Areeba Khan <khan6@stolaf.edu>
2019-12-07 17:31:52 -0600

    yaml story template

--- story.cpp
+++ story.cpp
@@ -2,6 +2,16 @@
 
 int main() {
   init();
-  add_yaml("file.yaml");
+// HEALTH AND MANA STATS::
+      print_at(1240,"  health  ************" );
+      print_at(1290, "  mana  ************");
+
+    print_at ( 1100, ">");
+
+    print_at(1200, "settings");
+    print_at(1220, "help");
+
+  add_yaml("story_template.yaml");
   quit();
+
 }
