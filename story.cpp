993acef06df0d76d453f978e1b551b53ab652abe
Areeba Khan <khan6@stolaf.edu>
2019-12-08 16:20:31 -0600

    death/help pages and the story.cpp

--- story.cpp
+++ story.cpp
@@ -5,13 +5,15 @@ int main() {
 // HEALTH AND MANA STATS::
       print_at(1240,"  health  ************" );
       print_at(1290, "  mana  ************");
-
+    print_at(1320,"back");
+    //death
+        print_at (1330,"menu");
     print_at ( 1100, ">");
-
     print_at(1200, "settings");
     print_at(1220, "help");
 
-  add_yaml("story_template.yaml");
+
+  add_yaml("fight_template.yaml");
   quit();
 
 }
