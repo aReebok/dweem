279dabbe9e32b084416f48eb1aa4eb0c7d584110
Areeba Khan <khan6@stolaf.edu>
2019-12-08 21:19:48 -0600

    update to story oof

--- story.cpp
+++ story.cpp
@@ -1,4 +1,6 @@
 #include"../react.h"
+#include <iostream>
+using namespace std;
 
 int main() {
   init();
@@ -20,44 +22,68 @@ int main() {
   print_at(1350,"items");
   print_at(1360,"talk");
   print_at(1370,"leave");
+
+  //STORY PLOT ---
+  print_at(2000,"Hello, you're running through the woods.\n\
+Cool you hear something down the road\n\
+you keep running lol ok.......");
+  print_at(3000,"OH! looks like you ran into a ENEMY.\n\
+Oh!! oh NOOOOOOOOOO!!!\n\
+But you can probably defeat him!\n\
+engage!");
+
+
   int state = 0;
   int prev = 0;
-
   
-  if (just_starting()) state = 0;
+  if (just_starting()) {
+    state = 0;
+    prev = 0;
+  }
   if (received_event()) {
     if (event_id_is("button_help")) {
       prev = state;
-      state = 1;
+      state = 99;
+      cerr << "prev / state : " << prev << " / " << state;
     }
     if (event_id_is("button_000")) state = prev;
+    if (event_id_is("button_next")) state = 2;
   }
 
   if (state == 0) {
+    prev = state;
     string bgimg = "https://i.pinimg.com/originals/89/fa/82/89fa825239b04567aa4b0b2bcf7c05b7.gif";
     string charimg = "https://cdna.artstation.com/p/assets/images/images/011/040/706/original/ismael-gil-sprite-walk.gif?1527559340";
-    string enmimg ="";
+    string enmimg = "";
+    int ParaIndex = 2000;
     signed int bgTop = 195; // 195
     signed int charTop = -80;
     signed int charRight = 110;
     add_yaml("story_template.yaml",{{"bgimg", bgimg},{"charimg", charimg},\
     {"bgTop",bgTop},{"charTop", charTop}, {"charRight", charRight},\
-    {"enmimg",enmimg}});
+    {"enmimg",enmimg},{"paraIndex",ParaIndex}});
 
   }
-  if (state == 1) add_yaml("help.yaml");
+  if (state == 99) add_yaml("help.yaml");
 
   if (state == 2) {
+    prev = state;
     string bgimg = "frame_1.png";
     string charimg = "";
     string enmimg ="enemy_s1.png";
+    int ParaIndex = 3000;
     signed int bgTop = 100; // 195
     signed int charTop = -80;
     signed int charRight = 110;
       add_yaml("story_template.yaml",{{"bgimg", bgimg},{"charimg", charimg},\
   {"bgTop",bgTop},{"charTop", charTop}, {"charRight", charRight},\
-  {"enmimg",enmimg}});
+  {"enmimg",enmimg},{"paraIndex",ParaIndex}});
   }
+
+if (state == 3) {
+  add_yaml("fight_template.yaml");
+  }
+
 
   //SCENE1:
   //string bgimg = "https://i.pinimg.com/originals/89/fa/82/89fa825239b04567aa4b0b2bcf7c05b7.gif";
