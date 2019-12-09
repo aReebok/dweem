a42e989d1d85d340e8daafb54c31bd92844a62bf
Areeba Khan <khan6@stolaf.edu>
2019-12-08 23:32:44 -0600

    Entity driver updated

--- EntityDriver.cpp
+++ EntityDriver.cpp
@@ -1,10 +1,13 @@
 #include "Entity.h"
+#include "Entity.cpp"
 
 int main () {
 	srand(time(NULL));
-	Player p;
-	Enemy e;
-	combined_stats(p,e);
-	init_combat(p,e);
-	cout << "-----------" <<endl;
+	Player p(10,10,1,2,"player",5);
+	p.add_item(Enchantment(1,5));
+	p.HUD();
+	// Enemy e;
+	// combined_stats(p,e);
+	// init_combat(p,e);
+	// cout << "-----------" <<endl;
 }
